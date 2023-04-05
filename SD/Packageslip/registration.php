d014619e0663c48fd7f89bdea6f8c1655b09edac
sandysd7022 <129860565+sandysd7022@users.noreply.github.com>
2023-04-05 11:45:27 +0530

    add in git

---
+++ SD/Packageslip/registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copyright © 2022 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'SD_Packageslip',
+    __DIR__
+);
